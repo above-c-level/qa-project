dfedc9358a69fca8d70d822fcffc903c13fb4400
above-c-level <itstimmitei@gmail.com>
2022-11-07 16:15:45 -0700

    Update ml_approach.py

--- ml_approach.py
+++ ml_approach.py
@@ -7,31 +7,29 @@ import numpy as np
 import torch
 from sklearn.model_selection import train_test_split
 
-from helpers import Bert, StoryHelper, read_answers, read_questions, read_story
+from helpers import Bert, StoryHelper, get_story_question_answers, text_f_score
 from qa import find_answer
 from terminalhelper import NEWLINE, VERBATIM, stringformat
 
+story_qas = get_story_question_answers('devset-official')
 
+scores = []
+for story, question_answer_pairs in story_qas:
+    story_text = story["TEXT"]
+    story_help = StoryHelper(story_text)
+    for question, answer in question_answer_pairs:
+        # The naive approach, average f score of 0.0034256
+        # Took 42 seconds to run
+        prediction = find_answer(question, story)
 
+        # Most similar BERT embedding, average f score of 0.0621117
+        # Also took 17 minutes to run, so presumably running on CPU
+        # prediction = story_help.most_similar_embedding(bert, question)
 
-# scores = []
-# for story, question_answer_pairs in story_qas:
-#     story_text = story["TEXT"]
-#     story_help = StoryHelper(story_text)
-#     for question, answer in question_answer_pairs:
-#         # The naive approach, average f score of 0.0034256
-#         # Took 42 seconds to run
-#         # prediction = find_answer(question, story)
-
-#         # Most similar BERT embedding, average f score of 0.0621117
-#         # Also took 17 minutes to run, so presumably running on CPU
-#         # prediction = story_help.most_similar_embedding(bert, question)
-
-#         # Most similar signature vector, average f score of 0.1869697
-#         # Took 3 minutes 17 seconds
-#         prediction = story_help.most_similar_signature(question)
-#         f_score = text_f_score(answer, prediction)
-#         scores.append(f_score)
-# print(f"Average f score: {np.mean(scores)}")
-# print(f"Median f score: {np.median(scores)}")
-# print(f"Standard deviation of f score: {np.std(scores)}")
+        # Most similar signature vector, average f score of 0.1869697
+        # Took 3 minutes 17 seconds
+        f_score = text_f_score(answer, prediction)
+        scores.append(f_score)
+print(f"Average f score: {np.mean(scores)}")
+print(f"Median f score: {np.median(scores)}")
+print(f"Standard deviation of f score: {np.std(scores)}")
