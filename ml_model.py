e2b95230c3513cbe4cb40b37a6f3db6a35eb1488
above-c-level <itstimmitei@gmail.com>
2022-11-14 15:10:31 -0700

    Added some possibly useful transformers

--- ml_model.py
+++ ml_model.py
@@ -1,82 +1,79 @@
-from sklearn.discriminant_analysis import (LinearDiscriminantAnalysis,
-                                           QuadraticDiscriminantAnalysis)
-from sklearn.ensemble import (AdaBoostRegressor, BaggingRegressor,
-                              ExtraTreesRegressor, GradientBoostingRegressor,
-                              HistGradientBoostingRegressor,
-                              RandomForestRegressor)
-from sklearn.kernel_ridge import KernelRidge
-from sklearn.linear_model import (
-    ARDRegression, BayesianRidge, ElasticNet, ElasticNetCV, HuberRegressor,
-    Lars, Lasso, LassoCV, LassoLars, LassoLarsCV, LassoLarsIC,
-    LinearRegression, LogisticRegression, LogisticRegressionCV,
-    OrthogonalMatchingPursuit, OrthogonalMatchingPursuitCV,
-    PassiveAggressiveRegressor, Perceptron, PoissonRegressor, Ridge, RidgeCV,
-    SGDRegressor, TheilSenRegressor, TweedieRegressor)
-from sklearn.naive_bayes import (BernoulliNB, GaussianNB)
-from sklearn.neighbors import KNeighborsRegressor, RadiusNeighborsRegressor
+import numpy as np
+from numpy.typing import ArrayLike
+from sklearn.base import TransformerMixin
+from sklearn.ensemble import (AdaBoostClassifier, BaggingClassifier,
+                              StackingClassifier, VotingClassifier)
+from sklearn.feature_selection import (SelectFdr, SelectFpr, SelectKBest,
+                                       SelectPercentile, VarianceThreshold,
+                                       chi2, f_classif, mutual_info_classif)
+from sklearn.naive_bayes import BernoulliNB, GaussianNB, MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier, RadiusNeighborsRegressor
 from sklearn.neural_network import MLPRegressor
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.svm import SVR, LinearSVR, NuSVR
-from sklearn.tree import DecisionTreeRegressor, ExtraTreeRegressor
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import (MaxAbsScaler, MinMaxScaler, Normalizer,
+                                   PolynomialFeatures, PowerTransformer,
+                                   RobustScaler, StandardScaler)
+from sklearn.utils import check_array
 
-# Average CV score on the training set was: 0.09033330719573207
-# model = make_pipeline(
-#     Binarizer(threshold=0.75),
-#     SelectPercentile(score_func=f_classif, percentile=51),
-#     BernoulliNB(alpha=1.0, fit_prior=True)
-# )
-model = LinearRegression()
-all_models = {
-    # Discriminant Analysis
-    "LinearDiscriminantAnalysis": LinearDiscriminantAnalysis(),
-    "QuadraticDiscriminantAnalysis": QuadraticDiscriminantAnalysis(),
-    # Ensemble Methods
-    "AdaBoostRegressor": AdaBoostRegressor(),
-    "BaggingRegressor": BaggingRegressor(),
-    "ExtraTreesRegressor": ExtraTreesRegressor(),
-    "GradientBoostingRegressor": GradientBoostingRegressor(),
-    "RandomForestRegressor": RandomForestRegressor(),
-    "HistGradientBoostingRegressor": HistGradientBoostingRegressor(),
-    # Kernel Ridge
-    "KernelRidge": KernelRidge(),
-    # Linear models, including baysesian methods and generalized linear models
-    "ARDRegression": ARDRegression(),
-    "BayesianRidge": BayesianRidge(),
-    "ElasticNet": ElasticNet(),
-    "ElasticNetCV": ElasticNetCV(),
-    "HuberRegressor": HuberRegressor(max_iter=200),
-    "Lars": Lars(),
-    "Lasso": Lasso(),
-    "LassoCV": LassoCV(),
-    "LassoLars": LassoLars(),
-    "LassoLarsCV": LassoLarsCV(),
-    "LassoLarsIC": LassoLarsIC(),
-    "LinearRegression": LinearRegression(),
-    "LogisticRegression": LogisticRegression(),
-    "LogisticRegressionCV": LogisticRegressionCV(),
-    "OrthogonalMatchingPursuit": OrthogonalMatchingPursuit(),
-    "OrthogonalMatchingPursuitCV": OrthogonalMatchingPursuitCV(),
-    "PassiveAggressiveRegressor": PassiveAggressiveRegressor(),
-    "Perceptron": Perceptron(),
-    "PoissonRegressor": PoissonRegressor(),
-    "Ridge": Ridge(),
-    "RidgeCV": RidgeCV(),
-    "SGDRegressor": SGDRegressor(),
-    "TheilSenRegressor": TheilSenRegressor(),
-    "TweedieRegressor": TweedieRegressor(),
-    # Naive Bayes
-    "BernoulliNB": BernoulliNB(),
-    "GaussianNB": GaussianNB(),
-    # Nearest Neighbors
-    "KNeighborsRegressor": KNeighborsRegressor(),
-    "RadiusNeighborsRegressor": RadiusNeighborsRegressor(),
-    # Multi-layer Perceptron
-    "MLPRegressor": MLPRegressor(),
-    # Support Vector Machines
-    "LinearSVR": LinearSVR(),
-    "NuSVR": NuSVR(),
-    "SVR": SVR(),
-    # Trees
-    "DecisionTreeRegressor": DecisionTreeRegressor(),
-    "ExtraTreeRegressor": ExtraTreeRegressor()
-}
+
+class ValueCount(TransformerMixin):
+    """
+    Takes in a numpy array and returns the same numpy array but with two
+    additional columns defined as the count of items equal to `value` in the
+    array and the count of items which are not equal to `value`.
+    """
+
+    def __init__(self, value: float = 0):
+        self.value = np.array(value)
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X: np.ndarray, y=None):
+        X = check_array(X)
+        feature_count = X.shape[1]
+        non_value = np.count_nonzero(X != self.value, axis=1)
+        value = feature_count - non_value
+        return np.c_[X, value, non_value]
+
+
+class ValueOverUnder(TransformerMixin):
+    """
+    Takes in a numpy array and returns the same numpy array but with two
+    additional columns defined as the count of items greater than `value` in the
+    array and the count of items which are less than `value`.
+    """
+
+    def __init__(self, value: float = 0):
+        self.value = np.array(value)
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X: np.ndarray, y=None):
+        X = check_array(X)
+        over = np.count_nonzero(X > self.value, axis=1)
+        under = np.count_nonzero(X < self.value, axis=1)
+        return np.c_[X, over, under]
+
+
+best_model = make_pipeline(
+    VarianceThreshold(),
+    MinMaxScaler(),
+    ValueCount(value=0),
+    MultinomialNB(alpha=1.0, fit_prior=True)
+                        # ("76Neighbors",
+                        #  KNeighborsClassifier(n_neighbors=76,
+                        #                       p=2,
+                        #                       weights="distance")),
+                        # ("SGD",
+                        #  SGDClassifier(alpha=0.0,
+                        #                eta0=1.0,
+                        #                fit_intercept=False,
+                        #                l1_ratio=1.0,
+                        #                learning_rate="invscaling",
+                        #                loss="epsilon_insensitive",
+                        #                penalty="elasticnet",
+                        #                power_t=1.0))]),
+)
